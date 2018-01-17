525319ec978049e86fe118b25b39de06e784c2a6
Bryan Schoeff <bryan.schoeff@axiaconsulting.net>
2018-01-17 10:22:52 -0500

    Move to testdouble.js from sinon

--- tests/printer-test.js
+++ tests/printer-test.js
@@ -1,10 +1,14 @@
 import test from 'ava'
-import sinon from 'sinon'
+import td from 'testdouble'
 
 let printer = require('../printer')
 
 test.beforeEach(t => {
-  t.context.log = sinon.spy(console, 'log')
+  td.replace(console, 'log')
+})
+
+test.afterEach(t => {
+  td.reset()
 })
 
 test('print', t => {
@@ -14,7 +18,9 @@ test('print', t => {
 
   printer.print(scores)
 
-  t.true(t.context.log.calledTwice)
-  t.is(' 1  [Pacific] Dallas Fuel (42)', t.context.log.getCall(0).args[0])
-  t.is(' 2  [Pacific] Los Angeles Gladiators (12)', t.context.log.getCall(1).args[0])
+  td.verify(console.log(td.matchers.anything()), { times: 2 })
+  td.verify(console.log(' 1  [Pacific] Dallas Fuel (42)'))
+  td.verify(console.log(' 2  [Pacific] Los Angeles Gladiators (12)'))
+
+  t.pass()
 })
