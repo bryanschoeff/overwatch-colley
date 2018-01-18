a50ce8e1b3065294044753fcefca9b94007cac8b
Bryan Schoeff <schoeff@gmail.com>
2018-01-17 19:35:29 -0500

    Make the call to addGames a single call

--- games.js
+++ games.js
@@ -2,43 +2,47 @@ let got = require('got')
 let owl = require('./owl.js')
 let scheduleUri = 'https://api.overwatchleague.com/schedule?locale=en_US'
 
-exports.loadGamesFromApi = new Promise((resolve, reject) => {
-  var results = []
+exports.loadGames = function (matrix) {
 
-  got(scheduleUri, { json: true }).then(response => {
-    // Just want the regular season; skip preseason (0) and take stages 1-4
-    let schedule = response.body.data.stages.slice(1, 4)
-    
-    schedule.forEach(stage => {
-      stage.matches.forEach(match => {
-        if (match.state === 'CONCLUDED') {
-          results.push([winner(match), loser(match)])
-        }
+  return new Promise((resolve, reject) => {
+    var results = []
+
+    got(scheduleUri, { json: true }).then(response => {
+      // Just want the regular season; skip preseason (0) and take stages 1-4
+      let schedule = response.body.data.stages.slice(1, 4)
+
+      schedule.forEach(stage => {
+        stage.matches.forEach(match => {
+          if (match.state === 'CONCLUDED') {
+            results.push([winner(match), loser(match)])
+          }
+        })
       })
+
+      addGamesToMatrix(results, matrix)
+      resolve()
+    }).catch(error => {
+      reject(error.response.body)
     })
-
-    resolve(results)
-  }).catch(error => {
-    reject(error.response.body)
   })
-})
+}
 
-exports.addGamesToMatrix = function (results, matrix) {
+function addGamesToMatrix(results, matrix) {
   results.forEach(result => {
     matrix.addGame(id(result[0]), id(result[1]))
   })
 }
 
-function id (owlId) {
+function id(owlId) {
   return owl.teams.find((element) => {
     return element.owlId === owlId
   }).id
 }
 
-function winner (match) {
+function winner(match) {
   return match.winner.id
 }
 
-function loser (match) {
+function loser(match) {
   return (match.winner.id === match.competitors[0].id) ? match.competitors[1].id : match.competitors[0].id
 }
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ let matrix = require('colley-rankings')(12)
 let printer = require('./printer.js')
 let games = require('./games.js')
 
-games.loadGamesFromApi.then((results) => {
-    games.addGamesToMatrix(results, matrix)
+games.loadGames(matrix).then(() => {
     printer.print(matrix.solve())
 }).catch((error) => {
     console.log(error)
